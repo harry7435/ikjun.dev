3fdc0966f8ab34706af530360a8fb52d0f60a430
Cho Ik Jun <harry960629@naver.com>
2025-03-15 16:59:26 +0900

    feat: add favicon, opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,29 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 
 export const metadata: Metadata = {
-  title: "ikjun dev",
+  title: "ikjun.dev",
   description: "개발자 조익준에 대한 포트폴리오 사이트입니다.",
+  openGraph: {
+    title: "ikjun.dev",
+    description: "개발자 조익준에 대한 포트폴리오 사이트입니다.",
+    type: "website",
+    locale: "ko_KR",
+    url: "https://ikjun-dev.vercel.app/",
+    siteName: "ikjun.dev",
+    images: [
+      {
+        url: "https://ikjun-dev.vercel.app/ikjun-dev.png",
+        width: 800,
+        height: 800,
+        alt: "ikjun.dev 프로필",
+      },
+    ],
+  },
+  twitter: {
+    title: "ikjun.dev",
+    description: "개발자 조익준에 대한 포트폴리오 사이트입니다.",
+    images: ["https://ikjun-dev.vercel.app/ikjun-dev.png"],
+  },
 };
 
 export default function RootLayout({
