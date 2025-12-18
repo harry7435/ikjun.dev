806871b8abe76af495824e1f2c525f7ce9649a3a
Cho Ik Jun <harry960629@naver.com>
2025-12-18 14:44:41 +0900

    feat: 노션 블로그 데이터를 항상 최신 데이터를 불러오도록 설정

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import { getAllPosts } from "@/lib/notion";
 import BlogClient from "./components/BlogClient";
 
-export const revalidate = 3600; // Revalidate every hour
+export const revalidate = 0;
 
 export default async function BlogPage() {
   const allContent = await getAllPosts();
