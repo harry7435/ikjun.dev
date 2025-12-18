3dbbc21a2ce38f1c35dbe0c4792e76d5c18192c2
Cho Ik Jun <harry960629@naver.com>
2025-12-18 14:11:05 +0900

    chore: contentlayer 설정 제거
    - next.config.ts에서 withContentlayer 제거
    - tsconfig.json에서 contentlayer 관련 경로 및 include 제거

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
-import { withContentlayer } from "next-contentlayer";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withContentlayer(nextConfig);
+export default nextConfig;
